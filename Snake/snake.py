9d7f7ee6df31be00b3814a53622f0f5b7dc58dd5
Finnomator <80774024+Finnomator@users.noreply.github.com>
2021-10-22 14:49:24 +0200

    Update snake.py

--- Snake/snake.py
+++ Snake/snake.py
@@ -63,22 +63,22 @@ def listenforinput():
     field[posappley][posapplex] = "[o]"
 
     while True:
-        if keyboard.is_pressed("w"):
+        if keyboard.is_pressed("w") or keyboard.is_pressed("up"):
             direction = "w"
             break
-        elif keyboard.is_pressed("a"):
+        elif keyboard.is_pressed("a") or keyboard.is_pressed("left"):
             direction = "a"
             break
-        elif keyboard.is_pressed("s"):
+        elif keyboard.is_pressed("s") or keyboard.is_pressed("down"):
             direction = "s"
             break
-        elif keyboard.is_pressed("d"):
+        elif keyboard.is_pressed("d") or keyboard.is_pressed("right"):
             direction = "d"
             break
 
         time.sleep(0.01)
 
-    while keyboard.is_pressed("w") or keyboard.is_pressed("a") or keyboard.is_pressed("s") or keyboard.is_pressed("d"):
+    while keyboard.is_pressed("w") or keyboard.is_pressed("a") or keyboard.is_pressed("s") or keyboard.is_pressed("d") or keyboard.is_pressed("right") or keyboard.is_pressed("down") or keyboard.is_pressed("left") or keyboard.is_pressed("up"):
         time.sleep(0.1)
     
     if direction == "w":
